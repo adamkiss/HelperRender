146cc9accb1d722ae36dcef930d3cc4ebcde8431
Adam Kiss <iam@adamkiss.com>
2012-07-29 00:47:47 +0200

    Renamed the HMTemplate (the non-singleton object) and created crude version of the Master

--- HelperMaster_Master.php
+++ HelperMaster_Master.php
@@ -5,3 +5,31 @@
  * @version 0.0.1
  * @since 2012-06-28
  */
+
+public class Master {
+
+	/////////////////////////////////// VARIABLES /////////////////////////////////////
+
+	private static $default_view = 'masters/default';
+	private static $masterTemplate = null;
+
+
+	///////////////////////////////// SINGLETON INIT /////////////////////////////////////
+
+	public static function init($view = false){
+		// create singe master template
+		self::$masterTemplate = new HMTemplate(if $view ? $view : $default_view);
+	}
+
+	///////////////////////////////////// OUTPUT /////////////////////////////////////
+
+	/**
+	 * Renders the master and returns it as a string
+	 *
+	 * @return contents of the master template
+	 */
+	public static function output() {
+		return self::$masterTemplate->render();
+	}
+
+}
