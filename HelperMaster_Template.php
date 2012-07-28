c4e9ae433b9b66818d8c821e5dd3c3f67cb894bc
Adam Kiss <iam@adamkiss.com>
2012-07-28 18:10:35 +0200

    Added data addition, private values and changed render to use $data instead of $variables

--- HelperMaster_Template.php
+++ HelperMaster_Template.php
@@ -7,7 +7,94 @@
  */
 
 class Template {
+
+	////////////////////////////////// VARIABLES /////////////////////////////////////
+
+	/**
+	 * Array of data to be passed to view
+	 */
+	private
+		$data = array();
+	/**
+	 * View path to be included
+	 */
+	private
+		$view = null;
+
+	/////////////////////////////// DATA MANIPULATION /////////////////////////////////////
 	
+	/**
+	 * Adds single item to the array (to be used by public methods)
+	 *
+	 * @param $key string     key, under which the $value is saved
+	 * @param $value mixed    piece of data to be saved
+	 * @param $force boolean  whether to force the save (if data exists, rewrite)
+	 * 
+	 */
+	private function addDataItem($key, $value, $force = false){
+		if ( !array_key_exists($key, $this->data) || $force === true ) {
+			$this->data[$key] = $value;
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Adds data to the array to be rendered
+	 *
+	 * @param $multiple boolean   whether $keyOrArray is single key, or array of k=>v pairs (false, true respectively)
+	 * @param $keyOrArray string  name under which the data will be available in the view, or array of k=>v pairs
+	 * @param $value mixed        value to be available in the view
+	 * @return int                number of added k=>v pairs
+	 */
+	public function addData($multiple, $keyOrArray, $value = null){
+		if ($multiple) {
+			
+			// multiple values in $keyOrArray
+			$result = 0;
+			foreach($keyOrArray as $key=>$value){
+				$result += $this->addDataItem($key, $value);
+			}
+			return $result;
+
+		} else {
+			
+			// single pair of key and value
+			return $this->addDataItem($keyOrArray, $value);
+
+		}
+	}
+
+	/**
+	 * Adds data to the array to be rendered, but forces rewrite if the key is taken
+	 *
+	 * @param $multiple boolean   whether $keyOrArray is single key, or array of k=>v pairs (false, true respectively)
+	 * @param $keyOrArray string  name under which the data will be available in the view, or array of k=>v pairs
+	 * @param $value mixed        value to be available in the view
+	 * @return int                number of added k=>v pairs
+	 */
+	public function forceAddData($id,$data){
+		if ($multiple) {
+			
+			// multiple values in $keyOrArray
+			$result = 0;
+			foreach($keyOrArray as $key=>$value){
+				$result += $this->addDataItem($key, $value, true);
+			}
+			return $result;
+
+		} else {
+			
+			// single pair of key and value
+			return $this->addDataItem($keyOrArray, $value, true);
+
+		}
+	}
+
+	/////////////////////////////// VIEW MANIPULATION /////////////////////////////////////
+
+
 	///////////////////////////////// RENDER METHOD /////////////////////////////////////
 
 	/**
@@ -21,7 +108,7 @@ class Template {
 		ob_start();
 
 			// prepare variables and include template
-			foreach ($this->variables as $k=>$v){ $$k = $v;	}
+			foreach ($this->data as $k=>$v){ $$k = $v;	}
 			include ($this->view);
 
 			// get the result and close the buffer
