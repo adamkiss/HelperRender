d03716bbee60f3fd3f8b1dc1fe07028ba3128f24
Adam Kiss <iam@adamkiss.com>
2012-07-28 18:09:48 +0200

    Basic render method of the Template object

--- HelperMaster_Template.php
+++ HelperMaster_Template.php
@@ -8,4 +8,28 @@
 
 class Template {
 	
+	///////////////////////////////// RENDER METHOD /////////////////////////////////////
+
+	/**
+	 * Renders view with data and returns it as a string
+	 * 
+	 * @return string Contents of a view (with data supplied)
+	 */
+	function render(){
+		 //init
+		$renderResult = '';
+		ob_start();
+
+			// prepare variables and include template
+			foreach ($this->variables as $k=>$v){ $$k = $v;	}
+			include ($this->view);
+
+			// get the result and close the buffer
+			$renderResult = ob_get_contents();
+		ob_end_clean();
+
+		// return string
+		return $renderResult;
+	}
+
 }
