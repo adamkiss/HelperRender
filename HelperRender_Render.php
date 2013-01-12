227860e5568db928efb2ea00ed5eb042a42cf8e2
Adam Kiss <iam@adamkiss.com>
2013-01-12 04:08:02 +0100

    Partial commit of transformation from HRTemplate to TemplateFile. Everything works except loop & auto mode

--- HelperRender_Render.php
+++ HelperRender_Render.php
@@ -8,259 +8,273 @@
 
 class Render {
 
-	/////////////////////////////////// VARIABLES /////////////////////////////////////
+  /////////////////////////////////// VARIABLES /////////////////////////////////////
 
-	/**
-	 * Default view to be used (if no other master is used)
-	 */
-	private static $default_view = '_master/application';	
-	/**
-	 * Master Template object
-	 */
-	private static $masterTemplate = null;
-	/**
-	 * Master variables (to be included in master call (even before it ))
-	 */
-	private static $render_master_variables = array();
-	/**
-	 * Global variables (to be included in each render:: call throughout request)
-	 */
-	private static $render_globals = array();
+  /**
+   * Default view to be used (if no other master is used)
+   */
+  private static $default_view = 'application';
+  /**
+   * View as selected by user (if one is given, null otherwise)
+   */
+  private static $user_selected_view = null;
+  /**
+   * Master Template object
+   */
+  private static $master_template = null;
 
-	////////////////////////////////// SINGLETON INIT /////////////////////////////////
+  ////////////////////////////////// SINGLETON INIT /////////////////////////////////
 
-	/**
-	 * Init function – initializes master template object, based on optional parameter
-	 *
-	 * @param string $view   optional view to be used as 'master'
-	 */
-	public static function init($view = false){
-		// create singe master template
-		self::$masterTemplate = new HRTemplate($view ? $view : self::$default_view);
-	}
+  /**
+   * Init handling function; if master template isn't created, it creates one
+   */
+  private static function init_master_on_demand(){
+    if (is_null(self::$master_template))
+      self::init_master();
+  }
 
-	//////////////////////////////// MASTER VARIABLES ////////////////////////////////
-	
-	/**
-	 * Adds single item to the global variables array
-	 *
-	 * @param string $key      key, under which the $value is saved
-	 * @param mixed $value     piece of data to be saved
-	 * 
-	 */
-	private static function add_master_variable($key, $value){
-		if (!array_key_exists($key, self::$render_master_variables)) {
-			self::$render_master_variables[$key] = $value;
-		} else {
-			trigger_error('Render::add_master_variable: You can\'t overwrite existing global key',E_WARNING);
-		}
-	}
+  ///////////////////////////////// FILENAME HELPER /////////////////////////////////
 
-	/**
-	 * Adds variables to array of global values (issues warning if it exists already)
-	 *
-	 * @param string|array $var   if string, it acts as a key in globals, array
-	 */
-	public static function add_master($var, $value=null){
-		// passed: array of values to add
-		if (is_array($var)){
-			foreach($var as $key=>$value){
-				self::add_master_variable($key, $value);
-			}
-			// give programmer notice of incorrect usage
-			if (!is_null($value)){ trigger_error('Render::add_master: $value not null, while $var is array. Incorrect data?', E_NOTICE); }
+  /**
+   * Translates slug into full path
+   *
+   * @param string $slug file name or short path
+   * @param string $ext  extension of the file (default php, otherwise unused)
+   * @return string
+   */
+  private static function get_path_from_slug($slug, $ext='php'){
+    return wire('config')->paths->views.$slug.'.'.$ext;
+  }
 
-		// passed: key/value combo
-		}else if (is_string($var)&&!is_null($value)){
-			self::add_master_variable($var, $value);
+  /**
+   * Switches underscore in $slug on/off
+   *
+   * @param boolean $underscore_first Test case with underscore first (partials)
+   * @param string $slug              Slug to test
+   * @return string
+   */
+  private static function switch_slug_underscore($slug){
+    $last_slash = strrpos($slug, '/');
+    if ($last_slash !== false){
+      $slug_path = substr($slug, 0, $last_slash+1);
+      $slug_file = substr($slug, $last_slash+1);
+    }else{
+      $slug_path = '';
+      $slug_file = $slug;
+    }
 
-		// anything else
-		}else{
-			throw new WireException('Incorrect data passed to Render::add_master(string|array $var, mixed $value)');
-		}
-	}
+    $slug_file = ($slug_file[0] === '_') ? substr($slug_file, 1) : '_'.$slug_file;
 
-	/**
-	 * Returns global variables' array
-	 *
-	 * @return array Render::$render_global
-	 */
-	public static function master_variables(){
-		return self::$render_master_variables;
-	}
+    return $slug_path.$slug_file;
+  }
 
-	//////////////////////////////// GLOBAL VARIABLES ////////////////////////////////
-	
-	/**
-	 * Adds single item to the global variables array
-	 *
-	 * @param string $key      key, under which the $value is saved
-	 * @param mixed $value     piece of data to be saved
-	 * 
-	 */
-	private static function add_global_variable($key, $value){
-		if (!array_key_exists($key, self::$render_globals)) {
-			self::$render_globals[$key] = $value;
-		} else {
-			trigger_error('Render::add_global: You can\'t overwrite existing global key',E_WARNING);
-		}
-	}
+  /**
+   * Tests slugs both with/out underscored slugs (partial/one & partial/_one)
+   * If one is found, it is returned. If not, original slug is returned
+   *
+   * @param boolean $underscore_first Test case with underscore first (partials)
+   * @param string $slug              Slug to test
+   * @return string
+   */
+  public static function test_slug_with_underscore($slug, $underscore_first=false){
+    $original_slug = $underscore_first ?
+      self::switch_slug_underscore($slug) : $slug;
+    $slug_to_return = false;
 
-	/**
-	 * Adds variables to array of global values (issues warning if it exists already)
-	 *
-	 * @param string|array $var   if string, it acts as a key in globals, array
-	 */
-	public static function add_global($var, $value=null){
-		// passed: array of values to add
-		if (is_array($var)){
-			foreach($var as $key=>$value){
-				self::add_global_variable($key, $value);
-			}
-			// give programmer notice of incorrect usage
-			if (!is_null($value)){ trigger_error('Render::add_global: $value not null, while $var is array. Incorrect data?', E_NOTICE); }
+    $first_test = self::get_path_from_slug($original_slug);
+    if (file_exists($first_test))
+      return $original_slug;
 
-		// passed: key/value combo
-		}else if (is_string($var)&&!is_null($value)){
-			self::add_global_variable($var, $value);
+    $new_slug = self::switch_slug_underscore($original_slug);
+    $second_test = self::get_path_from_slug($new_slug);
 
-		// anything else
-		}else{
-			throw new WireException('Incorrect data passed to Render::add_global(string|array $var, mixed $value)');
-		}
-	}
+    return file_exists($second_test) ? $new_slug : $original_slug;
+  }
 
-	/**
-	 * Returns global variables' array
-	 *
-	 * @return array Render::$render_global
-	 */
-	public static function globals(){
-		return self::$render_globals;
-	}
+  /**
+   * Finds (and returns) path to correct file for slug
+   *
+   * @param string $slug file name or short path
+   * @return string
+   */
+  public static function get_file($slug, $underscore_first=false){
+    return self::get_path_from_slug(
+      self::test_slug_with_underscore($slug, $underscore_first)
+    );
+  }
 
-	///////////////////////////////// PAGE EXTRACTION ////////////////////////////////
 
-	/**
-	 * Extracts the fields (and custom values assigned via getChanges()) from $page and returns
-	 * as an array prepared for inclusion in the render functions
-	 *
-	 * @param Page $page        page to have data extracted from
-	 * @return array            array of values in the page
-	 */
-	public static function extractValues(Page $page){
-		$data = array();
+  ////////////////////////// MASTER TEMPLATE MANIPULATION ///////////////////////////
 
-		// first: get fields
-		foreach($page->template->fields as $field){
-			$data[$field->name] = $page->get($field->name);
-			if(
-				$field->type instanceof FieldtypeDatetime
-			||$field->type instanceof FieldtypePageTitle
-			||$field->type instanceof FieldtypeText
-			||$field->type instanceof FieldtypeTextarea
-			){
-				$data[$field->name.'_UF'] = $page->getUnformatted($field->name);
-			}
-		}
+  /**
+   * Initializes master template object
+   */
+  private static function init_master(){
+    // create single master template
+    self::$master_template = new TemplateFile(
+      is_null(self::$view) ?
+        self::$view :
+        self::$default_view
+    );
+  }
 
-		// second: get 'data' and filter custom values
-		foreach ($page->getChanges() as $key){
-			if(!array_key_exists($key, $data)) {
-				$data[$key] = $page->get($key);
-			}
-		}
+  public static function set_master($filename){
+    self::init_master_on_demand();
 
-		return $data;
-	}
+    self::$master_template->setFilename(self::get_file($filename));
+  }
 
-	/////////////////////////////// TEMPLATE RENDERING ///////////////////////////////
+  /**
+   * WireData->set wrapper for master variables
+   *
+   * @param string $key      key, under which the $value is saved
+   *                         if it's "data", $value is to be understood as collection
+   * @param mixed $value     piece of data to be saved
+   */
+  public static function add_to_master($key, $value){
+    self::init_master_on_demand();
 
-	/**
-	 * Renders template with data, returns it as a string
-	 *
-	 * @param string $fileName   file name of template
-	 * @param string $data       data to be used to render the partial
-	 * @return string            the template content (with variables replaced)
-	 */
-	public static function partial($fileName, $data = array()){
-		$partial = new HRTemplate($fileName);
-		$partial->addData(true, $data);
-		return $partial->render();
-	}
+    self::$master_template->set($key, $value);
+  }
 
-	public static function page($additional_data = array()){
-		$page = Wire::getFuel('page');
-		return self::partial($page->template,
-			array_merge(self::extractValues($page), $additional_data)
-		);
-	}
+  //////////////////////////////// GLOBAL VARIABLES ////////////////////////////////
 
-	/**
-	 * Renders looped template with data and optional separator file, returns it as a string
-	 *
-	 * @param string $fileName               file name of template
-	 * @param string $data                   data to be used to render the partial
-	 * @param string $separatorFileName      data to be used to render the partial
-	 * @return string                        the template content (with variables replaced)
-	 */
-	public static function loop($fileName, $data = array(), $separatorFileName = false){
+  /**
+   * Adds variables to array of global values (issues warning if it exists already)
+   *
+   * @param string $var     key, if 'data', $value is parsed as array of k/v pairs
+   * @param mixed $value    value (or possibly array of k/v pairs)
+   */
+  public static function add_to_global($var, $value){
+    TemplateFile::setGlobal($key, $value, false);
+  }
 
-		$loopItem = new HRTemplate($fileName);
-		$loopSeparator = ($separatorFileName) ? new HRTemplate($separatorFileName) : false;
-		$result = array(); $count = 0; $itemsCount = count($data);
+  ///////////////////////////////// PAGE EXTRACTION ////////////////////////////////
 
-		foreach ($data as $item){
-			
-			$loopItem->removeAllData();
-			$loopItem->addData(true, $item);
-			$loopItem->addData(false, 'HM_Count', ++$count);
-			$result []= $loopItem->render();
+  /**
+   * Extracts the fields (and custom values assigned via getChanges()) from $page and returns
+   * as an array prepared for inclusion in the render functions
+   *
+   * @param Page $page        page to have data extracted from
+   * @return array            array of values in the page
+   */
+  public static function extractValues(Page $page){
+    $data = array();
 
-			if($loopSeparator && $count < $itemsCount) {
-				$loopSeparator->addData(false, 'HM_Count', $count);
-				$result []= $loopSeparator->render();
-			}
+    // first: get fields
+    foreach($page->template->fields as $field){
+      $data[$field->name] = $page->get($field->name);
+      if(
+        $field->type instanceof FieldtypeDatetime
+      ||$field->type instanceof FieldtypePageTitle
+      ||$field->type instanceof FieldtypeText
+      ||$field->type instanceof FieldtypeTextarea
+      ){
+        $data[$field->name.'_UF'] = $page->getUnformatted($field->name);
+      }
+    }
 
-		}
+    // second: get 'data' and filter custom values
+    foreach ($page->getChanges() as $key){
+      if(!array_key_exists($key, $data)) {
+        $data[$key] = $page->get($key);
+      }
+    }
 
-		return implode('', $result);
-	}
+    return $data;
+  }
 
-	public static function collection($fileName, $collection, $separatorFileName = false){
-		$collectionArray = array();
-		foreach($collection as $collectionItem) {
-			$collectionArray []= array('item'=>$collectionItem);
-		}
-		return self::loop($fileName, $collectionArray, $separatorFileName);
-	}
+  /////////////////////////////// TEMPLATE RENDERING ///////////////////////////////
 
-	///////////////////////////////////// OUTPUT /////////////////////////////////////
+  /**
+   * Renders template with data, returns it as a string
+   *
+   * @param string $filename   file name of template
+   * @param string $data       data to be used to render the partial
+   * @return string            the template content (with variables replaced)
+   */
+  public static function partial($filename, $data=array(), $underscore_first=true){
+    //hack: associative array check. It
+    if (is_array($data)){
+      $array_keys_count = count(array_filter(array_keys($data), 'is_string'));
+    }
+    $partial = new TemplateFile(self::get_file($filename, $underscore_first));
+    $partial->set('data', $data);
+    return $partial->render();
+  }
 
-	/**
-	 * Renders the master and returns it as a string
-	 *
-	 * @param array $data data to be pushed into the master
-	 * @return contents of the master template
-	 */
-	public static function master( $data = array() ) {
-		//make init option: if file wasn't initialized, initialize it for developer
-		if (!self::$masterTemplate) { self::init(); }
+  public static function page($additional_data=array(), $filename=null){
+    $page = Wire::getFuel('page');
+    return self::partial(is_null($filename) ? $page->template : $filename,
+      array_merge(self::extractValues($page), $additional_data), false
+    );
+  }
 
-		self::$masterTemplate->addData(true, $data);
-		self::$masterTemplate->addData(true, self::$render_master_variables);
-		return self::$masterTemplate->render();
-	}
+  /**
+   * Renders looped template with data and optional separator file, returns it as a string
+   *
+   * @param string $filename               file name of template
+   * @param string $data                   data to be used to render the partial
+   * @param string $separatorFileName      data to be used to render the partial
+   * @return string                        the template content (with variables replaced)
+   */
+  public static function loop($filename, $data = array(), $separatorFileName = false){
 
-	/**
-	 * Auto-renders master with $page fields in the $content, with possible additional data
-	 *
-	 * @param array $additionalData     optional data (good to use if you want 'auto' way, but need more data)
-	 */
-	public static function auto($additionalData = array()){
-		self::init();
-		echo self::master(array(
-			'content'=>Render::page($additionalData)
-		));
-	}
+    $loopItem = new HRTemplate($filename);
+    $loopSeparator = ($separatorFileName) ? new HRTemplate($separatorFileName) : false;
+    $result = array(); $count = 0; $itemsCount = count($data);
+
+    foreach ($data as $item){
+      
+      $loopItem->removeAllData();
+      $loopItem->addData(true, $item);
+      $loopItem->addData(false, 'HM_Count', ++$count);
+      $result []= $loopItem->render();
+
+      if($loopSeparator && $count < $itemsCount) {
+        $loopSeparator->addData(false, 'HM_Count', $count);
+        $result []= $loopSeparator->render();
+      }
+
+    }
+
+    return implode('', $result);
+  }
+
+  public static function collection($filename, $collection, $separatorFileName = false){
+    $collectionArray = array();
+    foreach($collection as $collectionItem) {
+      $collectionArray []= array('item'=>$collectionItem);
+    }
+    return self::loop($filename, $collectionArray, $separatorFileName);
+  }
+
+  ///////////////////////////////////// OUTPUT /////////////////////////////////////
+
+  /**
+   * Renders the master and returns it as a string
+   *
+   * @param array $data data to be pushed into the master
+   * @return contents of the master template
+   */
+  public static function master( $data = array() ) {
+    //make init option: if file wasn't initialized, initialize it for developer
+    if (!self::$masterTemplate) { self::init(); }
+
+    self::$masterTemplate->addData(true, $data);
+    self::$masterTemplate->addData(true, self::$render_master_variables);
+    return self::$masterTemplate->render();
+  }
+
+  /**
+   * Auto-renders master with $page fields in the $content, with possible additional data
+   *
+   * @param array $additionalData     optional data (good to use if you want 'auto' way, but need more data)
+   */
+  public static function auto($additionalData = array()){
+    self::init();
+    echo self::master(array(
+      'content'=>Render::page($additionalData)
+    ));
+  }
 }
